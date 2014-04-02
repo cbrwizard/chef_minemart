724e611a4422cfba4ae14f21e6cedf986b9bdf49
cbrwizard <cbrwizard@gmail.com>
2014-04-02 17:46:31 +0400

    working nginx cookbook to cbrwizard.com

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,13 +1,8 @@
-# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+cookbook_path    ["cookbooks", "site-cookbooks"]
+node_path        "nodes"
+role_path        "roles"
+environment_path "environments"
+data_bag_path    "data_bags"
+#encrypted_data_bag_secret "data_bag_key"
 
-current_dir = File.dirname(__FILE__)
-log_level                :info
-log_location             STDOUT
-node_name                "cbrwizard"
-client_key               "#{current_dir}/cbrwizard.pem"
-validation_client_name   "minemart-validator"
-validation_key           "#{current_dir}/minemart-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/minemart"
-cache_type               'BasicFile'
-cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            ["#{current_dir}/../cookbooks"]
+knife[:berkshelf_path] = "cookbooks"
