213586ec6d1f8588c9be4041fd583df8f25c41dc
cbrwizard <cbrwizard@gmail.com>
2014-04-03 14:21:12 +0400

    configured dependencies for mimenart

--- site-cookbooks/minemart/attributes/default.rb
+++ site-cookbooks/minemart/attributes/default.rb
@@ -1,2 +1,19 @@
-# ruby that will get installed and set to `rvm use default`.
-default['rvm']['default_ruby']      = "ruby-2.0.0-p353"
+default[:app][:name] = "minemart"
+default[:app][:web_dir] = "/var/data/www/apps/minemart"
+
+default[:rvm][:default_ruby] = "ruby-2.0.0-p353"
+default[:user][:name] = "root"
+
+default[:nginx][:version] = "1.4.4"
+default[:nginx][:default_site_enabled] = true
+default[:nginx][:source][:modules] = [
+  "nginx::http_gzip_static_module",
+  "nginx::http_ssl_module",
+  "nginx::http_gzip_static_module" ]
+default[:nginx][:passenger][:version] = "4.0.37"
+
+default[:run_list] = [
+  "recipe[nginx::source]",
+  "recipe[rvm::system]",
+  "recipe[minemart]"
+]
--- site-cookbooks/minemart/recipes/default.rb
+++ site-cookbooks/minemart/recipes/default.rb
@@ -1,6 +1,38 @@
-package "git"
+# fixes locales
+bash "fix_locales" do
+  user "root"
+  code <<-EOH
+    sudo locale-gen en_US ru_RU.UTF-8
+    dpkg-reconfigure locales
+  EOH
+end
 
-# nginx
+
+#postgresql
+package "postgresql-9.1"
+package "libpq-dev"
+package "postgresql-contrib"
+
+
+## adds memory for passenger
+#execute "increase_memory" do
+#  command 'sudo dd if=/dev/zero of=/swap bs=1M count=1024 && sudo mkswap /swap && sudo swapon /swap'
+#end
+
+#install main gems
+gem_package "rails" do
+  version "4.0.0"
+end
+gem_package 'capistrano'
+gem_package 'bundler'
+gem_package 'passenger' do
+  version node['nginx']['passenger']['version']
+end
+package 'nodejs'
+package 'libcurl4-openssl-dev'
+
+
+# set up nginx
 # creates logs
 %w(public logs).each do |dir|
   directory "#{node.app.web_dir}/#{dir}" do
@@ -23,10 +55,32 @@ nginx_site "#{node.app.name}.conf" do
   action :enable
 end
 
-# adds index.html
+# adds test index.html
 cookbook_file "#{node.app.web_dir}/public/index.html" do
   source "index.html"
   mode 0755
   owner node.user.name
 end
 
+
+# dependencies for opencv
+['libmagickwand-dev', 'libopencv-dev', 'build-essential', 'checkinstall', 'cmake', 'pkg-config', 'yasm', 'libtiff4-dev', 'libjpeg-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libdc1394-22-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'python-dev', 'python-numpy', 'libtbb-dev', 'libqt4-dev', 'libgtk2.0-dev' ].each do |apt|
+  package apt
+end
+
+
+# downloads and installs opencv
+bash "download_opencv" do
+  not_if {::File.exists?("/#{node.user.name}/tmp/opencv-2.4.6.1/build")}
+  cwd "/#{node.user.name}/tmp"
+  code <<-EOH
+    wget -O OpenCV-2.4.6.1 http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.6.1/opencv-2.4.6.1.tar.gz/download && tar -zxf OpenCV-2.4.6.1 && cd opencv-2.4.6.1 && mkdir build
+  EOH
+end
+
+bash "install_opencv" do
+  cwd "/#{node.user.name}/tmp/opencv-2.4.6.1/build"
+  code <<-EOH
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON .. && make && sudo make install && sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' &&  sudo ldconfig
+  EOH
+end
