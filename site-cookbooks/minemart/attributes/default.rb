7d3b70dc28157c745674c41a98dc926710fa8061
cbrwizard <cbrwizard@gmail.com>
2014-04-04 12:33:43 +0400

    updated ruby and added opencv recipe

--- site-cookbooks/minemart/attributes/default.rb
+++ site-cookbooks/minemart/attributes/default.rb
@@ -4,7 +4,7 @@ default[:app][:web_dir] = "/var/data/www/apps/minemart"
 
 default[:rvm][:user_installs] = [{
   :user => 'minemart',
-  :default_ruby => "ruby-2.0.0-p353"
+  :default_ruby => "ruby-2.1.1"
 }]
 
 default[:user][:name] = "minemart"
@@ -14,5 +14,10 @@ default[:nginx][:default_site_enabled] = true
 default[:nginx][:source][:modules] = [
   "nginx::http_gzip_static_module",
   "nginx::http_ssl_module",
-  "nginx::http_gzip_static_module" ]
+  "nginx::http_gzip_static_module",
+  "nginx::passenger"]
 default[:nginx][:passenger][:version] = "4.0.37"
+default[:nginx][:passenger][:ruby] = "/home/minemart/.rvm/wrappers/ruby-2.1.1/ruby"
+default[:nginx][:passenger][:root] = "/home/minemart/.rvm/gems/ruby-2.1.1/gems/passenger-4.0.37"
+
+default[:opencv][:version] = "2.4.6.1"
--- site-cookbooks/minemart/recipes/default.rb
+++ site-cookbooks/minemart/recipes/default.rb
@@ -13,10 +13,10 @@ package "libpq-dev"
 package "postgresql-contrib"
 
 
-## adds memory for passenger
-#execute "increase_memory" do
-#  command 'sudo dd if=/dev/zero of=/swap bs=1M count=1024 && sudo mkswap /swap && sudo swapon /swap'
-#end
+# adds memory for passenger
+execute "increase_memory" do
+  command 'sudo dd if=/dev/zero of=/swap bs=1M count=1024 && sudo mkswap /swap && sudo swapon /swap'
+end
 
 #install main gems
 gem_package "rails" do
@@ -61,27 +61,27 @@ cookbook_file "#{node.app.web_dir}/public/index.html" do
   action :create_if_missing
 end
 
-# dependencies for opencv
-['libmagickwand-dev', 'libopencv-dev', 'build-essential', 'checkinstall', 'cmake', 'pkg-config', 'yasm', 'libtiff4-dev', 'libjpeg-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libdc1394-22-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'python-dev', 'python-numpy', 'libtbb-dev', 'libqt4-dev', 'libgtk2.0-dev', 'qt5-default',  'qttools5-dev-tools' ].each do |apt|
-  package apt do
-    action :upgrade
-    options "--force-yes"
-  end
-end
+## dependencies for opencv
+#['libmagickwand-dev', 'libopencv-dev', 'build-essential', 'checkinstall', 'cmake', 'pkg-config', 'yasm', 'libtiff4-dev', 'libjpeg-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libdc1394-22-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'python-dev', 'python-numpy', 'libtbb-dev', 'libqt4-dev', 'libgtk2.0-dev', 'qt5-default',  'qttools5-dev-tools' ].each do |apt|
+#  package apt do
+#    action :upgrade
+#    options "--force-yes"
+#  end
+#end
 
-# downloads and installs opencv
-bash "download_opencv" do
-  not_if {::File.exists?("/home/#{node.user.name}/opencv-2.4.6.1/build")}
-  cwd "/home/#{node.user.name}"
-  code <<-EOH
-    wget -O OpenCV-2.4.6.1 http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.6.1/opencv-2.4.6.1.tar.gz/download && tar -zxf OpenCV-2.4.6.1 && cd opencv-2.4.6.1 && mkdir build
-  EOH
-end
-
-bash "install_opencv" do
-  not_if system("pkg-config --modversion opencv") == "2.4.6.1"
-  cwd "/home/#{node.user.name}/opencv-2.4.6.1/build"
-  code <<-EOH
-    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON .. && make && sudo make install && sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' &&  sudo ldconfig
-  EOH
-end
+## downloads and installs opencv
+#bash "download_opencv" do
+#  not_if {::File.exists?("/home/#{node.user.name}/opencv-2.4.6.1/build")}
+#  cwd "/home/#{node.user.name}"
+#  code <<-EOH
+#    wget -O OpenCV-2.4.6.1 http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.6.1/opencv-2.4.6.1.tar.gz/download && tar -zxf OpenCV-2.4.6.1 && cd opencv-2.4.6.1 && mkdir build
+#  EOH
+#end
+#
+#bash "install_opencv" do
+#  not_if system("pkg-config --modversion opencv") == "2.4.6.1"
+#  cwd "/home/#{node.user.name}/opencv-2.4.6.1/build"
+#  code <<-EOH
+#    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON .. && make && sudo make install && sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' &&  sudo ldconfig
+#  EOH
+#end
