84beeb4b85cbedb32b0cc763671963a9a16991c6
cbrwizard <cbrwizard@gmail.com>
2014-04-04 14:15:06 +0400

    added memory fix for opencv

--- site-cookbooks/minemart/attributes/default.rb
+++ site-cookbooks/minemart/attributes/default.rb
@@ -1,23 +1,24 @@
+default[:user][:name] = "minemart"
+
 default[:app][:name] = "minemart"
 default[:app][:domain] = "cbrwizard.com"
-default[:app][:web_dir] = "/var/data/www/apps/minemart"
+default[:app][:web_dir] = "/home/#{user[:name]}/apps/#{app[:name]}"
 
 default[:rvm][:user_installs] = [{
-  :user => 'minemart',
+  :user => "#{user[:name]}",
   :default_ruby => "ruby-2.1.1"
 }]
-
-default[:user][:name] = "minemart"
 
 default[:nginx][:version] = "1.4.4"
 default[:nginx][:default_site_enabled] = true
 default[:nginx][:source][:modules] = [
   "nginx::http_gzip_static_module",
   "nginx::http_ssl_module",
-  "nginx::http_gzip_static_module",
-  "nginx::passenger"]
+  "nginx::http_gzip_static_module"]
+
 default[:nginx][:passenger][:version] = "4.0.37"
-default[:nginx][:passenger][:ruby] = "/home/minemart/.rvm/wrappers/ruby-2.1.1/ruby"
-default[:nginx][:passenger][:root] = "/home/minemart/.rvm/gems/ruby-2.1.1/gems/passenger-4.0.37"
+default[:nginx][:passenger][:ruby] = "/home/#{user[:name]}/.rvm/wrappers/#{rvm[:default_ruby]}/wrappers/ruby"
+default[:nginx][:passenger][:root] = "/home/#{user[:name]}/.rvm/gems/#{rvm[:default_ruby]}/gems/passenger-#{nginx[:passenger][:version]}"
 
 default[:opencv][:version] = "2.4.6.1"
+default[:opencv][:url] = "https://github.com/Itseez/opencv/archive/#{opencv[:version]}.tar.gz"
--- site-cookbooks/minemart/recipes/default.rb
+++ site-cookbooks/minemart/recipes/default.rb
@@ -12,17 +12,17 @@ package "postgresql-9.1"
 package "libpq-dev"
 package "postgresql-contrib"
 
-
-# adds memory for passenger
-execute "increase_memory" do
-  command 'sudo dd if=/dev/zero of=/swap bs=1M count=1024 && sudo mkswap /swap && sudo swapon /swap'
-end
+# redis
+package "tcl8.5"
+package "redis-server"
 
 #install main gems
 gem_package "rails" do
   version "4.0.0"
 end
-gem_package 'capistrano'
+gem_package 'capistrano' do
+  version '2.15'
+end
 gem_package 'bundler'
 gem_package 'passenger' do
   version node['nginx']['passenger']['version']
@@ -61,27 +61,37 @@ cookbook_file "#{node.app.web_dir}/public/index.html" do
   action :create_if_missing
 end
 
-## dependencies for opencv
-#['libmagickwand-dev', 'libopencv-dev', 'build-essential', 'checkinstall', 'cmake', 'pkg-config', 'yasm', 'libtiff4-dev', 'libjpeg-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libdc1394-22-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'python-dev', 'python-numpy', 'libtbb-dev', 'libqt4-dev', 'libgtk2.0-dev', 'qt5-default',  'qttools5-dev-tools' ].each do |apt|
-#  package apt do
-#    action :upgrade
-#    options "--force-yes"
-#  end
-#end
+# adds memory for install stuff
+bash "add_swap" do
+  user "minemart"
+  code <<-EOH
+    sudo fallocate -l 1024m /var/spool/swap
+    sudo mkswap /var/spool/swap
+    sudo swapon /var/spool/swap
+  EOH
+end
 
-## downloads and installs opencv
-#bash "download_opencv" do
-#  not_if {::File.exists?("/home/#{node.user.name}/opencv-2.4.6.1/build")}
-#  cwd "/home/#{node.user.name}"
-#  code <<-EOH
-#    wget -O OpenCV-2.4.6.1 http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.6.1/opencv-2.4.6.1.tar.gz/download && tar -zxf OpenCV-2.4.6.1 && cd opencv-2.4.6.1 && mkdir build
-#  EOH
-#end
-#
-#bash "install_opencv" do
-#  not_if system("pkg-config --modversion opencv") == "2.4.6.1"
-#  cwd "/home/#{node.user.name}/opencv-2.4.6.1/build"
-#  code <<-EOH
-#    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON .. && make && sudo make install && sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' &&  sudo ldconfig
-#  EOH
-#end
+# dependencies for opencv
+['libmagickwand-dev', 'libopencv-dev', 'build-essential', 'checkinstall', 'cmake', 'pkg-config', 'yasm', 'libtiff4-dev', 'libjpeg-dev', 'libjasper-dev', 'libavcodec-dev', 'libavformat-dev', 'libswscale-dev', 'libdc1394-22-dev', 'libxine-dev', 'libgstreamer0.10-dev', 'libgstreamer-plugins-base0.10-dev', 'libv4l-dev', 'python-dev', 'python-numpy', 'libtbb-dev', 'libqt4-dev', 'libgtk2.0-dev', 'qt5-default',  'qttools5-dev-tools' ].each do |apt|
+  package apt do
+    action :upgrade
+    options "--force-yes"
+  end
+end
+
+# downloads and installs opencv
+bash "download_opencv" do
+  not_if {::File.exists?("/home/#{node.user.name}/opencv-#{node.opencv.version}/build")}
+  cwd "/home/#{node.user.name}"
+  code <<-EOH
+    wget -O OpenCV-#{node.opencv.version} #{node.opencv.url} && tar -zxf OpenCV-#{node.opencv.version} && cd opencv-#{node.opencv.version} && mkdir build
+  EOH
+end
+
+bash "install_opencv" do
+  not_if system("pkg-config --modversion opencv") == node.opencv.version
+  cwd "/home/#{node.user.name}/opencv-#{node.opencv.version}1/build"
+  code <<-EOH
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON .. && make && sudo make install && sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf' &&  sudo ldconfig
+  EOH
+end
